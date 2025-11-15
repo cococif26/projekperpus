04fb60e7f3f6f1a66da59b59c11858888bdbb029
Tanti Dwi Juliyanti <tdwijuliyanti@gmail.com>
2025-11-15 10:39:48 +0700

    Update fitur petugas di perpustakaan

--- lib/petugas.dart
+++ lib/petugas.dart
@@ -1,5 +1,23 @@
-import 'package:projekperpus/orang.dart';
+import 'orang.dart';
+import 'buku.dart';
+import 'sirkulasi.dart';
 
-class Petugas {
-  
+class Petugas extends Orang {
+  String jabatan;
+
+  Petugas(String id, String nama, this.jabatan) : super(id, nama);
+
+  // Method mengelola buku
+  void kelolaBuku(Buku buku, {bool ubahStatus = false}) {
+    if (ubahStatus) {
+      buku.ubahStatus();
+    }
+    print("Petugas $nama mengelola buku: ${buku.judul}");
+  }
+
+  // Method mengelola sirkulasi
+  void kelolaSirkulasi(Sirkulasi s) {
+    print("Petugas $nama memproses sirkulasi ID: ${s.idTransaksi}");
+    s.prosesSirkulasi();
+  }
 }
